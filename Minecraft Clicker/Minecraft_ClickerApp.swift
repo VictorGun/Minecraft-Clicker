119185b3f830021c0622bc3682c79517d1ab9038
VictorGun <Thunderboomboom7@gmail.com>
2022-11-28 09:26:37 -0600

    Initial Commit

---
+++ Minecraft Clicker/Minecraft_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minecraft_ClickerApp.swift
+//  Minecraft Clicker
+//
+//  Created by Victor Gunderson on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Minecraft_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
